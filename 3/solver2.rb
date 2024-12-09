d23b644ca44020d8ebf920bf103c77f31bc340df
Lewis Hyman <lewishymanpersonal@gmail.com>
2024-12-09 23:58:26 +1100

    day 5 + 6

--- 3/solver2.rb
+++ 3/solver2.rb
@@ -2,17 +2,18 @@ text = File.read("data.txt")
 
 regex = /(mul\(\d+,\d+\)|do\(\)|don't\(\))/
 capture = text.scan(regex)
-#puts capture
+
+puts capture
 val = 0
 current_state = :do
 
 capture.each do |arr_line|
   line = arr_line[0]
   if line == "do()"
-    puts "#{line} - #{val}"
+    #puts "#{line} - #{val}"
     current_state = :do
   elsif line == "don't()"
-    puts "#{line} - #{val}"
+    #puts "#{line} - #{val}"
     current_state = :dont
   else
     if current_state == :do
